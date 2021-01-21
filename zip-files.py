a156c8f90680e056f0e9caf5f7c526bb9c1e965f
Gurjit Singh <g.s@outlook.in>
2021-01-21 21:45:30 +0530

    new features yay!

--- zip-files.py
+++ zip-files.py
@@ -5,8 +5,11 @@
 
 
 import argparse
+import glob
+import itertools
 import math
 import pathlib
+import re
 import subprocess
 import sys
 
@@ -19,20 +22,36 @@ def parseArgs():
         else:
             raise argparse.ArgumentTypeError("Invalid Directory path")
 
+    def sepExts(exts):
+        if "," in exts:
+            return exts.strip().split(",")
+        else:
+            raise argparse.ArgumentTypeError("Invalid extensions list")
+
     parser = argparse.ArgumentParser(
-        description="Compress all files in a specified folder using 7z."
+        description="Compress all files in a specified folder(optionally in subfolders) into a split archive using 7z."
     )
     parser.add_argument(
         "-d", "--dir", required=True, help="Directory path", type=dirPath
     )
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
         "-s",
         "--split",
         nargs="?",
         default=None,
         const=300,
         type=int,
-        help="Maximum split size in MB, default is 300 MB",
+        help="Maximum split size in MB for multi-part archive, default is 300 MB",
+    )
+    group.add_argument(
+        "-sa",
+        "--standalone",
+        nargs="?",
+        default=None,
+        const=300,
+        type=int,
+        help="Maximum split size in MB for multiple standalone archives, default is 300 MB",
     )
     parser.add_argument(
         "-a",
@@ -40,21 +59,86 @@ def parseArgs():
         action="store_true",
         help=r"Use absolute 7z.exe path C:\Program Files\7-Zip\7z.exe",
     )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help=r"Process files recursively in all child directories.",
+    )
+    parser.add_argument(
+        "-e",
+        "--extensions",
+        required=False,
+        help="Comma separated file extensions(inclusive); end single extension with comma.",
+        type=sepExts,
+    )
+    parser.add_argument(
+        "-y",
+        "--dry",
+        action="store_true",
+        help=r"Dry run / Don't write anything to the disk.",
+    )
     pargs = parser.parse_args()
 
     return pargs
 
 
-getCmd = lambda dirPath, abs: [
+getCmd = lambda dirPath, abs, i=None: [
     "7z.exe" if not abs else r"C:\Program Files\7-Zip\7z.exe",
     "a",
-    f"{str(dirPath)}.zip",
-    str(dirPath),
+    f"{str(dirPath)}.zip"
+    if isinstance(i, type(None))
+    else f"{str(dirPath)} - Part {i + 1}.zip",
 ]
 
-getFileList = lambda dirPath: [x for x in dirPath.iterdir() if x.is_file()]
+
+def runCmd(cmd, dry):
+    print("\n---------------------------------------")
+    print("\nCreating archive:", cmd[2])
+    print("\n", cmd)
+    if not dry:
+        subprocess.run(cmd)
+    print("\n---------------------------------------\n")
+    input("\nPress Enter to continue...")
+
+
+
+addDots = lambda exts: [f".{x}" for x in exts]
+
+
+def getFileList(dirPath, exts):
+    if exts:
+        return [
+            x for x in dirPath.iterdir() if x.is_file() and x.suffix in addDots(exts)
+        ]
+    else:
+        return [x for x in dirPath.iterdir() if x.is_file()]
+
+
+def getFileListRec(dirPath, exts):
+    if exts:
+        fList = list(
+            itertools.chain.from_iterable(
+                [glob.glob(f"{dirPath}/**/*.{f}", recursive=True) for f in exts]
+            )
+        )
+    else:
+        fList = glob.glob(f"{dirPath}/**/*", recursive=True)
+
+    return [pathlib.Path(x) for x in fList]
+
+# getFileList = lambda dirPath: [x for x in dirPath.iterdir() if x.is_file()]
+
+# getFileListRec = lambda dirPath, exts: [
+#     pathlib.Path(x)
+#     for x in itertools.chain.from_iterable(
+#         [glob.glob(f"{dirPath}/**/*.{f}", recursive=True) for f in exts]
+#     )
+# ]
 
 bytesToMB = lambda bytes: math.ceil(bytes / float(1 << 20))
+
+stringify = lambda paths: [str(x) for x in paths]
 
 
 def getSize(totalSize, maxSplit):
@@ -67,7 +151,7 @@ def getSize(totalSize, maxSplit):
             fSize = splitSize
             return i, splitSize
     if fSize == 0:
-        raise Exception("file size either too small or too big")
+        return 1, totalSize
 
 
 def getFileSizes(fileList):
@@ -77,32 +161,49 @@ def getFileSizes(fileList):
     return totalSize
 
 
+def nSort(s, _nsre=re.compile("([0-9]+)")):
+    return [int(text) if text.isdigit() else text.lower() for text in _nsre.split(s)]
+
+
+def printAndExit(msg):
+    print(msg)
+    sys.exit()
+
+
 def main(pargs):
 
     dirPath = pargs.dir.resolve()
 
-    fileList = getFileList(dirPath)
+    if pargs.recursive:
+        fileList = getFileListRec(dirPath, pargs.extensions)
+    else:
+        fileList = getFileList(dirPath, pargs.extensions)
+
+    fileList = sorted(fileList, key=lambda k: nSort(str(k.stem)))
 
     if not fileList:
-        print("Nothing to do.")
-        sys.exit()
+        printAndExit("Nothing to do.")
 
-    totalSize = getFileSizes(fileList)
+    totalSize = bytesToMB(getFileSizes(fileList))
 
-    cmd = getCmd(dirPath, pargs.abs)
+    split = pargs.split or pargs.standalone
 
-    if pargs.split:
-        splitSize = getSize(bytesToMB(totalSize), pargs.split)[1]
-        cmd.append(f"-v{splitSize}m")
+    splitInfo = getSize(totalSize, split)
 
-    print("\n--------------------------------------------")
-    print("\n", cmd)
-    print(f"\nTotal size of source files { bytesToMB(totalSize) } MB")
-    if pargs.split:
-        print(f"\nSplit size: {splitSize} MB")
-    print("\n--------------------------------------------\n")
-    subprocess.run(cmd)
-    input("\nPress Enter to continue...")
+    if pargs.standalone:
+        splitSize = splitInfo[1]
+        splitNum = math.ceil(len(fileList) / splitInfo[0])
+        for i in range(splitInfo[0]):
+            partFiles = fileList[splitNum * i : splitNum * (i + 1)]
+            cmd = getCmd(dirPath, pargs.abs, i)
+            cmd.extend(stringify(partFiles))
+            runCmd(cmd, pargs.dry)
+
+    elif pargs.split:
+        splitSize = splitInfo[1]
+        cmd = getCmd(dirPath, pargs.abs)
+        cmd.extend([str(dirPath), f"-v{splitSize}m"])
+        runCmd(cmd, pargs.dry)
 
 
 main(parseArgs())
